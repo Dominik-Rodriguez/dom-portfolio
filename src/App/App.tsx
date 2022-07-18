81a2fface5291ef9ebb4c525e0be76896080c0f0
Dominik-Rodriguez <54704566+Dominik-Rodriguez@users.noreply.github.com>
2022-07-17 21:41:43 -0600

    adding more routes and tinkering with tsParticles

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,17 +1,34 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
+import Particles from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
 import { Routes, Route, Outlet } from 'react-router-dom';
 import './App.css';
 import PageContainer from './PageContainer/PageContainer';
 import LandingPage from './LandingPage/LangingPage';
+import { Container } from 'react-dom';
+import AboutMe from './AboutMe/AboutMe';
 
 function App() {
+	// const particlesInit = async (main: any) => {
+	// 	console.log(main);
+	// 	await loadFull(main);
+	// };
+	
+	// const particlesLoaded = (container: Container) => {
+	// 	console.log(container);
+	// };
   return (
+    <React.Fragment>
+    {/* <Particles id='particles' init={particlesInit} loaded={particlesLoaded} /> */}
     <Routes>
-      <Route path='/' element={ <PageContainer /> }>
-        <Route index element={ <LandingPage /> }></Route>
-      </Route>
+    	<Route path='/' element={ <PageContainer /> }>
+    	    <Route index element={ <LandingPage /> }></Route>
+			<Route path='/about' element={ <AboutMe /> }></Route>
+    	</Route>
     </Routes>
+
+    </React.Fragment>
   );
 }
 
